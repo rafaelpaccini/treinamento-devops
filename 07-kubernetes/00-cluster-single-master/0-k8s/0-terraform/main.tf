1d2a8a4b8442b2b30309a2d4b37b1ff629c6e36e
rafaelpaccini <rafael.mamede90@gmail.com>
2021-11-18 21:21:25 +0000

    aulas

--- 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
+++ 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
@@ -1,15 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
+  region = "sa-east-1"
 }
 
 resource "aws_instance" "maquina_master" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.medium"
-  key_name      = "treinamento-turma1_itau"
+  subnet_id = "subnet-06efd40357f45eadf"
+  ami           = "ami-0e66f5495b4efdd0f"
+  instance_type = "t2.large"
+  associate_public_ip_address = true
+  key_name      = "itau-treinamento"
   tags = {
     Name = "k8s-master"
   }
@@ -20,11 +18,13 @@ resource "aws_instance" "maquina_master" {
 }
 
 resource "aws_instance" "workers" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
-  key_name      = "treinamento-turma1_itau"
+  subnet_id = "subnet-06efd40357f45eadf"
+  ami           = "ami-0e66f5495b4efdd0f"
+  instance_type = "t2.medium"
+  associate_public_ip_address = true
+  key_name      = "itau-treinamento"
   tags = {
-    Name = "k8s-node-${count.index}"
+    Name = "k8s-node-${count.index + 1}"
   }
   vpc_security_group_ids = [aws_security_group.acessos_workers_single_master.id]
   count         = 3
@@ -33,14 +33,25 @@ resource "aws_instance" "workers" {
 resource "aws_security_group" "acessos_master_single_master" {
   name        = "acessos_master_single_master"
   description = "acessos_workers_single_master inbound traffic"
-
+  vpc_id = "vpc-08eb8eaf23779e64b"
   ingress = [
     {
       description      = "SSH from VPC"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+    {
+      description      = "porta 30000 para todos"
+      from_port        = 30000
+      to_port          = 30000
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids = null,
       security_groups: null,
@@ -54,7 +65,7 @@ resource "aws_security_group" "acessos_master_single_master" {
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = [
-        "sg-0de8412f761f70f50",
+        "sg-0fcd885c093c4d191",
       ]
       self             = false
       to_port          = 0
@@ -97,14 +108,14 @@ resource "aws_security_group" "acessos_master_single_master" {
 resource "aws_security_group" "acessos_workers_single_master" {
   name        = "acessos_workers_single_master"
   description = "acessos_workers_single_master inbound traffic"
-
+  vpc_id = "vpc-08eb8eaf23779e64b"
   ingress = [
     {
       description      = "SSH from VPC"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids = null,
       security_groups: null,
@@ -118,7 +129,7 @@ resource "aws_security_group" "acessos_workers_single_master" {
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = [
-        "sg-0c8c7bdac4e2dbfb7",
+        "${aws_security_group.acessos_master_single_master.id}",
       ]
       self             = false
       to_port          = 0
@@ -148,7 +159,8 @@ resource "aws_security_group" "acessos_workers_single_master" {
 # terraform refresh para mostrar o ssh
 output "maquina_master" {
   value = [
-    "master - ${aws_instance.maquina_master.public_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${aws_instance.maquina_master.public_dns}"
+    "master - ${aws_instance.maquina_master.public_ip} - ssh -i ~/aws/key01 ubuntu@${aws_instance.maquina_master.public_dns}",
+    aws_security_group.acessos_workers_single_master.id
   ]
 }
 
@@ -156,6 +168,6 @@ output "maquina_master" {
 output "aws_instance_e_ssh" {
   value = [
     for key, item in aws_instance.workers :
-      "worker ${key+1} - ${item.public_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${item.public_dns}"
+      "worker ${key+1} - ${item.public_ip} - ssh -i ~/aws/key01 ubuntu@${item.public_dns}"
   ]
 }
